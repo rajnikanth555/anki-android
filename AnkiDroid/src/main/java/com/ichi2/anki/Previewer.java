5ba8d4b3ddd32bbca17086e951e556c2983d56fb
automation <automation@mygamma.io>
2021-04-05 12:39:03 +0530

    commitMessage : Preview progress

--- AnkiDroid/src/main/java/com/ichi2/anki/Previewer.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/Previewer.java
@@ -18,22 +18,15 @@
 
 package com.ichi2.anki;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.ichi2.libanki.Collection;
-import com.ichi2.libanki.Utils;
 import com.ichi2.themes.Themes;
 
-import java.util.HashSet;
-import java.util.List;
-
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
 import timber.log.Timber;
 
 /**
@@ -45,27 +38,13 @@ public class Previewer extends AbstractFlashcardViewer {
     private long[] mCardList;
     private int mIndex;
     private boolean mShowingAnswer;
-
-    /** Communication with Browser */
-    private boolean mReloadRequired;
-    private boolean mNoteChanged;
-
-
-    @CheckResult
-    @NonNull
-    public static Intent getPreviewIntent(Context context, int index, long[] cardList) {
-        Intent intent = new Intent(context, Previewer.class);
-        intent.putExtra("index", index);
-        intent.putExtra("cardList", cardList);
-        return intent;
-    }
+    private SeekBar progressSeekBar;
+    private TextView progressText;
+    private boolean mEnableLiveSeek = true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (showedActivityFailedScreen(savedInstanceState)) {
-            return;
-        }
         Timber.d("onCreate()");
         super.onCreate(savedInstanceState);
 
@@ -75,8 +54,6 @@ public class Previewer extends AbstractFlashcardViewer {
         if (savedInstanceState != null){
             mIndex = savedInstanceState.getInt("index", mIndex);
             mShowingAnswer = savedInstanceState.getBoolean("showingAnswer", mShowingAnswer);
-            mReloadRequired = savedInstanceState.getBoolean("reloadRequired");
-            mNoteChanged = savedInstanceState.getBoolean("noteChanged");
         }
 
         if (mCardList.length == 0 || mIndex < 0 || mIndex > mCardList.length - 1) {
@@ -88,40 +65,70 @@ public class Previewer extends AbstractFlashcardViewer {
         // Ensure navigation drawer can't be opened. Various actions in the drawer cause crashes.
         disableDrawerSwipe();
         startLoadingCollection();
+        initPreviewProgress();
     }
+
+
+    private void initPreviewProgress() {
+        progressSeekBar = findViewById(R.id.preview_progress_seek_bar);
+        progressText = findViewById(R.id.preview_progress_text);
+        LinearLayout progressLayout = findViewById(R.id.preview_progress_layout);
+
+        //Show layout only when the cardList is bigger than 1
+        if (mCardList.length > 1) {
+            progressLayout.setVisibility(View.VISIBLE);
+            progressSeekBar.setMax(mCardList.length - 1);
+            setSeekBarListener();
+            updateProgress();
+        }
+    }
+
+
+    private void setSeekBarListener() {
+        progressSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mIndex = progress;
+                    updateProgress();
+                    if (mEnableLiveSeek) {
+                        mCurrentCard = getCol().getCard(mCardList[mIndex]);
+                        displayCardQuestion();
+                    }
+                }
+            }
+
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Mandatory override, but unused
+            }
+
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (!mEnableLiveSeek) {
+                    if (mIndex >= 0 && mIndex < mCardList.length) {
+                        mCurrentCard = getCol().getCard(mCardList[mIndex]);
+                        displayCardQuestion();
+                    }
+                }
+            }
+        });
+    }
+
 
     @Override
     protected void onCollectionLoaded(Collection col) {
         super.onCollectionLoaded(col);
         mCurrentCard = col.getCard(mCardList[mIndex]);
-
-        displayCardQuestion();
         if (mShowingAnswer) {
+            displayCardQuestion();
             displayCardAnswer();
+        } else {
+            displayCardQuestion();
         }
-
         showBackIcon();
-    }
-
-    /** Given a new collection of card Ids, find the 'best' valid card given the current collection
-     * We define the best as searching to the left, then searching to the right of the current element
-     * This occurs as many cards can be deleted when editing a note (from the Card Template Editor) */
-    private int getNextIndex(List<Long> newCardList) {
-        HashSet<Long> validIndices = new HashSet<>(newCardList);
-
-        for (int i = mIndex; i >= 0; i--) {
-            if (validIndices.contains(mCardList[i])) {
-                return newCardList.indexOf(mCardList[i]);
-            }
-        }
-
-        for (int i = mIndex + 1; i < validIndices.size(); i++) {
-            if (validIndices.contains(mCardList[i])) {
-                return newCardList.indexOf(mCardList[i]);
-            }
-        }
-
-        throw new IllegalStateException("newCardList was empty");
     }
 
 
@@ -135,62 +142,14 @@ public class Previewer extends AbstractFlashcardViewer {
     protected void initLayout() {
         super.initLayout();
         mTopBarLayout.setVisibility(View.GONE);
-
-        findViewById(R.id.answer_options_layout).setVisibility(View.GONE);
-
-        mPreviewButtonsLayout.setVisibility(View.VISIBLE);
-        mPreviewButtonsLayout.setOnClickListener(mToggleAnswerHandler);
-
-        mPreviewPrevCard.setOnClickListener(mSelectScrollHandler);
-        mPreviewNextCard.setOnClickListener(mSelectScrollHandler);
-
-        if (animationEnabled()) {
-            int resId = Themes.getResFromAttr(this, R.attr.hardButtonRippleRef);
-            mPreviewButtonsLayout.setBackgroundResource(resId);
-            mPreviewPrevCard.setBackgroundResource(R.drawable.item_background_light_selectable_borderless);
-            mPreviewNextCard.setBackgroundResource(R.drawable.item_background_light_selectable_borderless);
-        }
     }
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_edit) {
-            editCard();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        setResult(RESULT_OK, getResultIntent());
-        super.onBackPressed();
-    }
-
-
-    @Override
-    protected void onNavigationPressed() {
-        setResult(RESULT_OK, getResultIntent());
-        super.onNavigationPressed();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.previewer, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        outState.putLongArray("cardList", mCardList);
+        super.onSaveInstanceState(outState);
         outState.putInt("index", mIndex);
         outState.putBoolean("showingAnswer", mShowingAnswer);
-        outState.putBoolean("reloadRequired", mReloadRequired);
-        outState.putBoolean("noteChanged", mNoteChanged);
-        super.onSaveInstanceState(outState);
     }
 
 
@@ -198,7 +157,7 @@ public class Previewer extends AbstractFlashcardViewer {
     protected void displayCardQuestion() {
         super.displayCardQuestion();
         mShowingAnswer = false;
-        updateButtonsState();
+        updateButtonState();
     }
 
 
@@ -207,103 +166,104 @@ public class Previewer extends AbstractFlashcardViewer {
     protected void displayCardAnswer() {
         super.displayCardAnswer();
         mShowingAnswer = true;
-        updateButtonsState();
+        updateButtonState();
     }
 
 
+    // we don't want the Activity title to be changed.
     @Override
-    protected void hideEaseButtons() {
-        /* do nothing */
-    }
+    protected void updateScreenCounts() { /* do nothing */ }
 
+
+    // No Gestures!
     @Override
-    protected void displayAnswerBottomBar() {
-        /* do nothing */
-    }
+    protected void executeCommand(int which) { /* do nothing */ }
 
 
-    @Override
-    public boolean executeCommand(int which) {
-        /* do nothing */
-        return false;
-    }
-
-
-    @Override
-    protected void performReload() {
-        mReloadRequired = true;
-        List<Long> newCardList = getCol().filterToValidCards(mCardList);
-
-        if (newCardList.isEmpty()) {
-            finishWithoutAnimation();
-            return;
-        }
-
-        mIndex = getNextIndex(newCardList);
-        mCardList = Utils.collection2Array(newCardList);
-        mCurrentCard = getCol().getCard(mCardList[mIndex]);
-        displayCardQuestion();
-    }
-
-
-    @Override
-    protected void onEditedNoteChanged() {
-        super.onEditedNoteChanged();
-        mNoteChanged = true;
-    }
-
-
-    private final View.OnClickListener mSelectScrollHandler = new View.OnClickListener() {
+    private View.OnClickListener mSelectScrollHandler = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if (view.getId() == R.id.preview_previous_flashcard) {
-                mIndex--;
-            } else if (view.getId() == R.id.preview_next_flashcard) {
-                mIndex++;
-            }
-
-            mCurrentCard = getCol().getCard(mCardList[mIndex]);
-            displayCardQuestion();
-        }
-    };
-
-    private final View.OnClickListener mToggleAnswerHandler = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
             if (mShowingAnswer) {
+                // If we are showing the answer, any click will show a question...
+                if (view.getId() == R.id.flashcard_layout_ease2) {
+                    // ...but if they clicked "forward" we need to move to the next card first
+                    mIndex++;
+                    mCurrentCard = getCol().getCard(mCardList[mIndex]);
+                }
                 displayCardQuestion();
+                updateProgress();
             } else {
+                // If we are showing the question, any click will show an answer...
+                if (view.getId() == R.id.flashcard_layout_ease1) {
+                    // ...but if they clicked "reverse" we need to go to the previous card first
+                    mIndex--;
+                    mCurrentCard = getCol().getCard(mCardList[mIndex]);
+                }
                 displayCardAnswer();
             }
         }
     };
 
-    private void updateButtonsState() {
-        mPreviewToggleAnswerText.setText(mShowingAnswer ? R.string.hide_answer : R.string.show_answer);
 
+    private void updateProgress() {
+        if (progressSeekBar.getProgress() != mIndex) {
+            progressSeekBar.setProgress(mIndex);
+        }
+
+        String progress = (mIndex + 1) + "/" + mCardList.length;
+        progressText.setText(progress);
+    }
+
+
+    private void updateButtonState() {
         // If we are in single-card mode, we show the "Show Answer" button on the question side
-        // and hide navigation buttons.
+        // and hide all the button s on the answer side.
         if (mCardList.length == 1) {
-            mPreviewPrevCard.setVisibility(View.GONE);
-            mPreviewNextCard.setVisibility(View.GONE);
+            if (!mShowingAnswer) {
+                mFlipCardLayout.setVisibility(View.VISIBLE);
+            } else {
+                findViewById(R.id.answer_options_layout).setVisibility(View.GONE);
+                mFlipCardLayout.setVisibility(View.GONE);
+                hideEaseButtons();
+            }
             return;
         }
 
-        boolean prevBtnDisabled = mIndex <= 0;
-        boolean nextBtnDisabled = mIndex >= mCardList.length - 1;
+        mFlipCardLayout.setVisibility(View.GONE);
+        mEase1Layout.setVisibility(View.VISIBLE);
+        mEase2Layout.setVisibility(View.VISIBLE);
+        mEase3Layout.setVisibility(View.GONE);
+        mEase4Layout.setVisibility(View.GONE);
 
-        mPreviewPrevCard.setEnabled(!prevBtnDisabled);
-        mPreviewNextCard.setEnabled(!nextBtnDisabled);
+        final int[] background = Themes.getResFromAttr(this, new int[] {R.attr.hardButtonRef});
+        final int[] textColor = Themes.getColorFromAttr(this, new int[] {R.attr.hardButtonTextColor});
 
-        mPreviewPrevCard.setAlpha(prevBtnDisabled ? 0.38F : 1);
-        mPreviewNextCard.setAlpha(nextBtnDisabled ? 0.38F : 1);
-    }
+        mNext1.setTextSize(30);
+        mEase1.setVisibility(View.GONE);
+        mNext1.setTextColor(textColor[0]);
+        mEase1Layout.setOnClickListener(mSelectScrollHandler);
+        mEase1Layout.setBackgroundResource(background[0]);
 
-    @NonNull
-    private Intent getResultIntent() {
-        Intent intent = new Intent();
-        intent.putExtra("reloadRequired", mReloadRequired);
-        intent.putExtra("noteChanged", mNoteChanged);
-        return intent;
+        mNext2.setTextSize(30);
+        mEase2.setVisibility(View.GONE);
+        mNext2.setTextColor(textColor[0]);
+        mEase2Layout.setOnClickListener(mSelectScrollHandler);
+        mEase2Layout.setBackgroundResource(background[0]);
+
+        if (mIndex == 0 && !mShowingAnswer) {
+            mEase1Layout.setEnabled(false);
+            mNext1.setText("-");
+        } else {
+            mEase1Layout.setEnabled(true);
+            mNext1.setText("<");
+        }
+
+        if (mIndex == mCardList.length - 1 && mShowingAnswer) {
+            mEase2Layout.setEnabled(false);
+            mNext2.setText("-");
+        } else {
+            mEase2Layout.setEnabled(true);
+            mNext2.setText(">");
+        }
     }
 }
