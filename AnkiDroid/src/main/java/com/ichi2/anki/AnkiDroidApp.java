389d4504bafe756ba9bf6395738e76ebb2626751
automation <automation@mygamma.io>
2021-04-05 12:22:43 +0530

    commitMessage : Render manual and support page HTML / include in app

--- AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.java
@@ -24,8 +24,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.net.Uri;
@@ -35,13 +33,11 @@ import android.os.LocaleList;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
-import androidx.core.content.pm.PackageInfoCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.util.Log;
 import android.view.ViewConfiguration;
 import android.webkit.CookieManager;
-import android.webkit.WebView;
 
 import com.ichi2.anki.analytics.AnkiDroidCrashReportDialog;
 import com.ichi2.anki.contextmenu.AnkiCardContextMenu;
@@ -72,14 +68,10 @@ import org.acra.config.ToastConfigurationBuilder;
 import org.acra.sender.HttpSender;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import androidx.webkit.WebViewCompat;
 import timber.log.Timber;
 import static timber.log.Timber.DebugTree;
 
@@ -201,7 +193,7 @@ public class AnkiDroidApp extends Application {
 
 
     public static boolean isInitialized() {
-        return sInstance != null;
+        return sInstance == null;
     }
 
 
@@ -220,6 +212,9 @@ public class AnkiDroidApp extends Application {
     }
 
 
+
+
+
     /**
      * Get the ACRA ConfigurationBuilder - use this followed by setting it to modify the config
      * @return ConfigurationBuilder for the current ACRA config
@@ -236,8 +231,6 @@ public class AnkiDroidApp extends Application {
     private void setAcraConfigBuilder(CoreConfigurationBuilder acraCoreConfigBuilder) {
         this.acraCoreConfigBuilder = acraCoreConfigBuilder;
         ACRA.init(this, acraCoreConfigBuilder);
-        ACRA.getErrorReporter().putCustomData("WEBVIEW_VER_NAME", fetchWebViewInformation().get("WEBVIEW_VER_NAME"));
-        ACRA.getErrorReporter().putCustomData("WEBVIEW_VER_CODE", fetchWebViewInformation().get("WEBVIEW_VER_CODE"));
     }
 
     @Override
@@ -614,6 +607,7 @@ public class AnkiDroidApp extends Application {
         }
     }
 
+
     /**
      * Check whether l is the currently set language code
      * @param l ISO2 language code
@@ -717,22 +711,6 @@ public class AnkiDroidApp extends Application {
                     break;
             }
         }
-    }
-
-    @NonNull
-    private HashMap<String, String> fetchWebViewInformation() {
-        HashMap<String, String> webViewInfo = new HashMap<>();
-        webViewInfo.put("WEBVIEW_VER_NAME", "");
-        webViewInfo.put("WEBVIEW_VER_CODE", "");
-        try {
-            PackageManager packageManager = getPackageManager();
-            PackageInfo pi = WebViewCompat.getCurrentWebViewPackage(this);
-            webViewInfo.put("WEBVIEW_VER_NAME", pi.versionName);
-            webViewInfo.put("WEBVIEW_VER_CODE", String.valueOf(PackageInfoCompat.getLongVersionCode(pi)));
-        } catch (Throwable e) {
-            Timber.w(e);
-        }
-        return webViewInfo;
     }
 
 }
