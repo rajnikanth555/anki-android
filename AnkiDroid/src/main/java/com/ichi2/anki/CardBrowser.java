e11c74402cb579901519d7b825eb36b7166eb092
automation555 <59799061+automation555@users.noreply.github.com>
2021-05-21 16:09:31 +0530

    Update CardBrowser.java

--- AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/CardBrowser.java
@@ -173,6 +173,7 @@ public class CardBrowser extends NavigationDrawerActivity implements
     private static final int EDIT_CARD = 0;
     private static final int ADD_NOTE = 1;
     private static final int PREVIEW_CARDS = 2;
+    int a;
 
     private static final int DEFAULT_FONT_SIZE_RATIO = 100;
     // Should match order of R.array.card_browser_order_labels
